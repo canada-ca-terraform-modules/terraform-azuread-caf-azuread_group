6dcae9356c8f9ac27575aebba9585bf44f353aeb
Bernard Maltais <bmaltais@gmail.com>
2020-08-14 18:40:52 +0000

    Update to terraform 0.13 style

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,23 @@
-output "azuread_groups" {
+output "object" {
   description = "Returns the full set of azuread_group created"
 
-  value = azuread_group.groups
+  value = azuread_group.group
+}
+
+output "name" {
+  description = "Returns the display name for the Group."
+
+  value = azuread_group.group.name
+}
+
+output "owners" {
+  description = "Returns a set of owners who own this Group."
+
+  value = azuread_group.group.owners
+}
+
+output "id" {
+  description = "Return the Object ID of the Group."
+
+  value = azuread_group.group.id
 }
