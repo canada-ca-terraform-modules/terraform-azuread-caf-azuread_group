12da6154d81ccaf712ba1d65bd3b994fec109c65
Bernard Maltais <bernard.maltais@canada.ca>
2020-11-15 02:24:07 +0000

    Update

--- test/main.tf
+++ test/main.tf
@@ -17,11 +17,9 @@ locals {
   azuread_groupsMap = {
     L1_Subscription_Owners = {
       userDefinedString = "${local.template_name}_L1_Subscription_Owners"
-      owners            = {}
     },
     L1_Subscription_Contributors = {
       userDefinedString = "${local.template_name}_L1_Subscription_Contributors"
-      owners            = {}
     },
   }
 }
@@ -32,5 +30,4 @@ module azuread_groups_L1 {
 
   env               = "ScTc"
   userDefinedString = each.value.userDefinedString
-  owners            = each.value.owners
 }
