aec1b9cf8c1f9bc912c0974e2b82ca157e0b4dc7
bernardmaltais <bernard.maltais@ssc-spc.gc.ca>
2021-09-07 11:14:58 -0400

    Update to comply to new provider release (#9)

--- module.tf
+++ module.tf
@@ -1,9 +1,16 @@
-data azuread_users group_owners {
+data "azuread_users" "group_owners" {
   user_principal_names = var.owners
 }
 
-resource azuread_group group {
+resource "azuread_group" "group" {
   display_name            = local.name
   owners                  = data.azuread_users.group_owners.object_ids
   prevent_duplicate_names = var.prevent_duplicate_names
+  mail_enabled            = var.mail_enabled
+  security_enabled        = var.security_enabled
+  description             = var.description
+  assignable_to_role      = var.assignable_to_role
+  behaviors               = var.behaviors
+  mail_nickname           = var.mail_nickname
+  types                   = var.types
 }
--- variables.tf
+++ variables.tf
@@ -24,3 +24,45 @@ variable "prevent_duplicate_names" {
   type        = bool
   default     = true
 }
+
+variable "mail_enabled" {
+  description = "Whether the group is a mail enabled, with a shared group mailbox. At least one of mail_enabled or security_enabled must be specified. Only Microsoft 365 groups can be mail enabled (see the types property)."
+  type        = bool
+  default     = false
+}
+
+variable "mail_nickname" {
+  description = "The mail alias for the group, unique in the organisation. Required for mail-enabled groups. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+
+variable "security_enabled" {
+  description = "Whether the group is a mail enabled, with a shared group mailbox. At least one of mail_enabled or security_enabled must be specified. Only Microsoft 365 groups can be mail enabled (see the types property)."
+  type        = bool
+  default     = true
+}
+
+variable "description" {
+  description = "The description for the group."
+  type        = string
+  default     = ""
+}
+
+variable "assignable_to_role" {
+  description = "Indicates whether this group can be assigned to an Azure Active Directory role. Can only be true for security-enabled groups. Changing this forces a new resource to be created."
+  type        = bool
+  default     = false
+}
+
+variable "behaviors" {
+  description = "A set of behaviors for a Microsoft 365 group. Possible values are AllowOnlyMembersToPost, HideGroupInOutlook, SubscribeNewGroupMembers and WelcomeEmailDisabled. See official documentation for more details. Changing this forces a new resource to be created."
+  type        = any
+  default     = []
+}
+
+variable "types" {
+  description = "A set of group types to configure for the group. The only supported type is Unified, which specifies a Microsoft 365 group. Required when mail_enabled is true. Changing this forces a new resource to be created."
+  type        = any
+  default     = []
+}
